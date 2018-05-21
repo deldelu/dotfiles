1176e6961b4b5063674ae60db36f4a0dc2a4f708
Aymeric Sorek <delt0x@protonmail.com>
2018-05-21 11:10:51 +0200

    lock screen date larger

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -2,16 +2,16 @@
 
 # Suspend dunst and lock, then resume dunst when unlocked.
 i3lock -n -i $HOME/Pictures/galaxy.jpg --tiling \
-    --ring-width=2 \
+    --ring-width=3 \
     --insidecolor=00000000 --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=8c1f3dff --bshlcolor=8c1f3dff --separatorcolor=00000000 \
     --insidevercolor=8c1f3dff \
     --insidewrongcolor=d23c3dff \
     --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
-    --indpos="x+150:y+980" \
+    --indpos="x+140:y+965" \
     --radius=15 --veriftext="" --wrongtext="" --noinputtext="" --indicator \
     --ignore-empty-password \
-    --clock --timecolor=ffffffff --timestr="%H:%M" --timepos="x+200:y+980" \
-    --timesize=30 --time-align=1 \
+    --clock --timecolor=ffffffff --timestr="%H:%M" --timepos="x+200:y+970" \
+    --timesize=60 --time-align=1 \
     --datecolor=ffffffff --datestr="%A, %d %B" --datepos="x+200:y+1000" \
-    --datesize=16 --date-align=1
+    --datesize=20 --date-align=1
