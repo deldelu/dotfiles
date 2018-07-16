9577466c3a50038cc2e5549a0bb65ed4fc306388
Aymeric Sorek <delt0x@protonmail.com>
2018-07-17 01:42:38 +0200

    Position and color fix for i3lock and discord have fixed workspace

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 maincolor=d52a5eff
-insidecolor=00000066
+insidecolor=000000aa
 
 # Suspend dunst and lock, then resume dunst when unlocked.
 i3lock -n -i $HOME/Pictures/cyberpunk.jpg --tiling \
     --clock --timecolor=ffffffff --timestr="%H:%M" --timepos="ix+0:iy+0" \
     --time-font=Hack --timesize=54 --time-align=0 \
-    --datecolor=bbbbbbff --datestr="%d/%m/%Y" \
-    --time-font=Hack --datesize=18 --date-align=0 --datepos="ix+0:iy+35"  \
+    --datecolor=ccccccff --datestr="%d/%m/%Y" \
+    --time-font=Hack --datesize=20 --date-align=0 --datepos="ix+0:iy+40"  \
     --indicator --ring-width=4 --radius=140 \
     --insidecolor=$insidecolor --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=$maincolor --bshlcolor=$maincolor --separatorcolor=00000000 \
@@ -17,6 +17,7 @@ i3lock -n -i $HOME/Pictures/cyberpunk.jpg --tiling \
     --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
     --veriftext="verifying..." --verifsize=22 --verifpos="ix+0:iy+10" \
     --verif-font=Hack --verifcolor=ffffffff  \
-    --wrongtext="ACCESS DENIED" --wrong-font=Hack --wrongcolor=c53341ff \
+    --wrongtext="ACCESS DENIED" --wrongpos="ix+0:iy+10" \
+    --wrong-font=Hack --wrongcolor=c53341ff \
     --noinputtext="no input" \
     --ignore-empty-password 
