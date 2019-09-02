3c7001ce58f7c5583af727bbe1ac0160fd6bed38
Aymeric Sorek <delt0x@protonmail.com>
2019-09-03 00:08:51 +0200

    New lock screen with no notification

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
 
-maincolor=d52a5eff
-insidecolor=000000aa
+maincolor=f17050ff
+insidecolor=00000000
+
+# Enable compton's fade-in effect so that the lockscreen gets a nice fade-in
+# effect.
+#dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
+#    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:false
+
+killall -SIGUSR1 dunst # pause
 
 # Suspend dunst and lock, then resume dunst when unlocked.
-i3lock -n -i $HOME/Pictures/cyberpunk.jpg --tiling \
-    --clock --timecolor=ffffffff --timestr="%H:%M" --timepos="ix+0:iy+0" \
-    --time-font=Hack --timesize=54 --time-align=0 \
-    --datecolor=ccccccff --datestr="%d/%m/%Y" \
-    --time-font=Hack --datesize=20 --date-align=0 --datepos="ix+0:iy+40"  \
-    --indicator --ring-width=4 --radius=140 \
+i3lock -n -i $HOME/Pictures/battle.jpg --tiling --indpos="x+85:y+980" \
+    --clock --timecolor=ffffffff --timestr="%H:%M" --timepos="ix+50:iy+00" \
+    --time-font=Roboto --timesize=54 --time-align=1 \
+    --datecolor=ccccccff --datestr="%A, %d %B %Y" \
+    --time-font=Roboto --datesize=20 --date-align=1 --datepos="ix+50:iy+40"  \
+    --indicator --ring-width=5 --radius=10 \
     --insidecolor=$insidecolor --ringcolor=ffffffff --line-uses-inside \
     --keyhlcolor=$maincolor --bshlcolor=$maincolor --separatorcolor=00000000 \
     --insidewrongcolor=$insidecolor \
     --insidevercolor=$insidecolor \
     --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
-    --veriftext="verifying..." --verifsize=22 --verifpos="ix+0:iy+10" \
-    --verif-font=Hack --verifcolor=ffffffff  \
-    --wrongtext="ACCESS DENIED" --wrongpos="ix+0:iy+10" \
-    --wrong-font=Hack --wrongcolor=c53341ff \
-    --noinputtext="no input" \
+    --veriftext="verifying..." --verifsize=20 --verifpos="ix+50:iy+7" \
+    --verif-font=Roboto --verifcolor=ffffffff --verif-align=1 \
+    --wrongtext="Close the gate!" --wrongpos="ix+50:iy+7" \
+    --wrong-font=Roboto --wrongcolor=$maincolor --wrongsize=20 --wrong-align=1 \
+    --noinputtext="" \
     --ignore-empty-password 
+
+killall -SIGUSR2 dunst #resume
+
+# Revert compton's config changes.
+#sleep 0.2
+#dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
+#    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:true
