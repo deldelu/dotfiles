6ac87a6aae4b0f79f678dee82725efde08feb2a7
Aymeric Sorek <delt0x@protonmail.com>
2019-09-03 21:34:47 +0200

    Compton update conf

--- config/i3/scripts/lock.sh
+++ config/i3/scripts/lock.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-maincolor=f17050ff
+maincolor=c646bbff
 insidecolor=00000000
 
+dpy=$(echo -n "$DISPLAY" | tr -c '[:alnum:]' _)
 # Enable compton's fade-in effect so that the lockscreen gets a nice fade-in
 # effect.
-#dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
-#    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:false
+dbus-send --print-reply --dest=com.github.chjj.compton.${dpy} / \
+    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:false
 
 killall -SIGUSR1 dunst # pause
 
 # Suspend dunst and lock, then resume dunst when unlocked.
-i3lock -n -i $HOME/Pictures/battle.jpg --tiling --indpos="x+85:y+980" \
+i3lock -n -i $HOME/Pictures/circle.jpg --tiling --indpos="x+85:y+130" \
     --clock --timecolor=ffffffff --timestr="%H:%M" --timepos="ix+50:iy+00" \
     --time-font=Roboto --timesize=54 --time-align=1 \
     --datecolor=ccccccff --datestr="%A, %d %B %Y" \
@@ -24,7 +25,7 @@ i3lock -n -i $HOME/Pictures/battle.jpg --tiling --indpos="x+85:y+980" \
     --ringvercolor=ffffffff --ringwrongcolor=ffffffff \
     --veriftext="verifying..." --verifsize=20 --verifpos="ix+50:iy+7" \
     --verif-font=Roboto --verifcolor=ffffffff --verif-align=1 \
-    --wrongtext="Close the gate!" --wrongpos="ix+50:iy+7" \
+    --wrongtext="Wrong password." --wrongpos="ix+50:iy+7" \
     --wrong-font=Roboto --wrongcolor=$maincolor --wrongsize=20 --wrong-align=1 \
     --noinputtext="" \
     --ignore-empty-password 
@@ -32,6 +33,6 @@ i3lock -n -i $HOME/Pictures/battle.jpg --tiling --indpos="x+85:y+980" \
 killall -SIGUSR2 dunst #resume
 
 # Revert compton's config changes.
-#sleep 0.2
-#dbus-send --print-reply --dest=com.github.chjj.compton.${DISPLAY/:/_} / \
-#    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:true
+sleep 0.2
+dbus-send --print-reply --dest=com.github.chjj.compton.${dpy} / \
+    com.github.chjj.compton.opts_set string:no_fading_openclose boolean:true
